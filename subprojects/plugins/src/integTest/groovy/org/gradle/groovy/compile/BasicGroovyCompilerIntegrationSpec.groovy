496caab0a33b3635a72f995d5614894644f17bf1
Marc Philipp <marc@gradle.com>
2018-10-01 11:13:07 +0200

    Always use configured annotationProcessorPath
     - Don't use an empty path for `-proc:none` because it is also used by
       compiler plugins
     - Don't support setting it to `null` anymore.
    
    Resolves #6573.

--- subprojects/plugins/src/integTest/groovy/org/gradle/groovy/compile/BasicGroovyCompilerIntegrationSpec.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/groovy/compile/BasicGroovyCompilerIntegrationSpec.groovy
@@ -134,7 +134,7 @@ abstract class BasicGroovyCompilerIntegrationSpec extends MultiVersionIntegratio
 
         buildFile << """
             compileGroovy {
-                options.compilerArgs << '-proc:none'
+                options.annotationProcessorPath = files()
             }
         """
 
@@ -280,7 +280,7 @@ abstract class BasicGroovyCompilerIntegrationSpec extends MultiVersionIntegratio
 
         buildFile << """
             compileGroovy {
-                options.compilerArgs << '-proc:none'
+                options.annotationProcessorPath = files()
             }
         """
 
